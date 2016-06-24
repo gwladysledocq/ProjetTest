921a494890698ac4d35faeeb0ef1c54b6714f419
inti0215 <inti0215@INTI-0215>
2016-06-24 10:39:48 +0200

    deuxieme commit

---
+++ TestGitHub/com/myapp/entities/Classe.java
@@ -0,0 +1,32 @@
+package com.myapp.entities;
+
+public class Classe {
+	
+	private String monAttr1;
+	private String monAttri2;
+	
+	public Classe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Classe(String monAttr1, String monAttri2) {
+		super();
+		this.monAttr1 = monAttr1;
+		this.monAttri2 = monAttri2;
+	}
+	public String getMonAttr1() {
+		return monAttr1;
+	}
+	public void setMonAttr1(String monAttr1) {
+		this.monAttr1 = monAttr1;
+	}
+	public String getMonAttri2() {
+		return monAttri2;
+	}
+	public void setMonAttri2(String monAttri2) {
+		this.monAttri2 = monAttri2;
+	}
+	
+	
+
+}
